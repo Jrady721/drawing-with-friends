3da25cf9632d5f6727c1721f32be929466858682
jrady <jrady721@gmail.com>
2019-04-30 09:54:59 +0900

    register

--- src/application/controller/RegisterController.java
+++ src/application/controller/RegisterController.java
@@ -1,5 +1,7 @@
 package application.controller;
 
+import application.model.User;
+import application.model.UserDAO;
 import application.util.Util;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -25,8 +27,25 @@ public class RegisterController {
         System.out.println("회원가입");
 
         String username = insertUsername.getText();
+        String password = insertPassword.getText();
+        String confirmPassword = insertConfirmPassword.getText();
 
-        Util.Alert("회원가입 성공", "회원가입에 성공했습니다", username + "님 환영합니다", Alert.AlertType.INFORMATION, btnRegister);
+        if (username.equals("") || password.equals("") || confirmPassword.equals("")) {
+            Util.Alert("회원가입 실패", "입력 오류", "폼을 정확히 입력해주세요", Alert.AlertType.WARNING, btnRegister);
+        } else {
+            if (!password.equals(confirmPassword)) {
+                Util.Alert("회원가입 실패", "입력 오류", "비밀번호와 비밀번호 확인이 틀립니다.", Alert.AlertType.WARNING, btnRegister);
+            } else {
+                User user = UserDAO.searchUser(username);
+                if (user != null) {
+                    Util.Alert("회원가입 실패", "아이디 중복", "이미 사용중인 아이디입니다.", Alert.AlertType.WARNING, btnRegister);
+                } else {
+                    UserDAO.insertUser(username, password);
+                    Util.Alert("회원가입 성공", "회원가입에 성공했습니다", username + "님 환영합니다", Alert.AlertType.INFORMATION, btnRegister);
+                    Util.Move("login", "로그인", btnRegister);
+                }
+            }
+        }
     }
 
     // 회원가입 페이지 -> 로그인 페이지 이동 버튼
